3e0f258ce1b7ba220cbd7d78c532e2a2cd1b3a99
Harry Pulvirenti <>
2020-05-29 12:00:02 +0200

    Reformat

--- mobile-app/src/main/java/com/ttp/app/MainActivity.kt
+++ mobile-app/src/main/java/com/ttp/app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.ttp.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
