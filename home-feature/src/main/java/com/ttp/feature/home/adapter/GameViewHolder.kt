f4632ef849d214a52f9f631a0c76a251f1d5f065
Harry Pulvirenti <>
2020-10-17 18:04:06 +0200

    Update coil to 1.0.0-rc3

--- home-feature/src/main/java/com/ttp/feature/home/adapter/GameViewHolder.kt
+++ home-feature/src/main/java/com/ttp/feature/home/adapter/GameViewHolder.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import coil.api.load
+import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.google.android.exoplayer2.ExoPlayer
 import com.ttp.entities.Game
